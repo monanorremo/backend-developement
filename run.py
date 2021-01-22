1c53ce9c0e5ea96ef7bacc752b2e9c0c76fa8a4e
monanorremo <mona.norremo@gmail.com>
2021-01-22 21:53:07 +0000

    Flas & Data template

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template
 
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -25,9 +25,8 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 
-
 if __name__ == "__main__":
     app.run(
-        host= os.environ.get("IP", "0.0.0.0"), 
-        port= int(os.environ.get("PORT", "5000")), 
-        debug= True)
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
